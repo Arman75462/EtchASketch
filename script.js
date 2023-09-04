ae949101df9aadd2581ebf1f2ba6e5b5e811a867
Arman <armangulistani@gmail.com>
2023-09-04 14:29:54 -0400

    Almost finished HTML/CSS design

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 "use strict";
 
-const container = document.querySelector(".etch-a-sketch");
+/* Selectors */
+const container = document.querySelector(".main__grid");
+const colorPicker = document.getElementById("color-picker");
+
+// Based on chosen color, change the circle's color
+colorPicker.addEventListener("input", () => {
+  const selectedColor = colorPicker.value;
+  colorPicker.style.backgroundColor = selectedColor;
+});
 
 // Create 256 div elements and add them to the container
 for (let i = 1; i <= 256; i++) {
   const div = document.createElement("div");
-  div.className = `grid-item-${i}`;
-  div.classList.add("grid-item");
+  div.className = `main__grid-item grid-item-${i}`; // Add both classes
   container.appendChild(div);
 }
