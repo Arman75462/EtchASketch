baa1a1598f9c1a6c11a3fd75c3cd9647d7866360
Arman <armangulistani@gmail.com>
2023-09-05 12:19:25 -0400

    Clear, eraser adn color button work

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 /* Selectors */
 const container = document.querySelector(".main__grid");
 const colorPicker = document.getElementById("color-picker");
+const eraser = document.getElementById("eraser");
+const clearButton = document.getElementById("clear-button");
 
 // Based on chosen color, change the circle's color
 colorPicker.addEventListener("input", () => {
@@ -11,8 +13,43 @@ colorPicker.addEventListener("input", () => {
 });
 
 // Create 256 div elements and add them to the container
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= 4096; i++) {
   const div = document.createElement("div");
-  div.className = `main__grid-item grid-item-${i}`; // Add both classes
+  div.className = `main__grid-items grid-item-${i}`; // Add both classes
   container.appendChild(div);
 }
+
+const gridItems = document.querySelectorAll(".main__grid-items");
+
+// Depending on clicked button, executes order
+gridItems.forEach((item) => {
+  let eraserState = false;
+
+  eraser.addEventListener("click", function () {
+    eraserState = !eraserState; // Toggle eraser state
+  });
+
+  item.addEventListener("mousemove", function (event) {
+    const selectedColor = colorPicker.value;
+    item.style.backgroundColor = selectedColor;
+
+    const clickedItem = event.target;
+
+    if (eraserState) {
+      clickedItem.style.backgroundColor = "whitesmoke"; // Erase if eraser is active
+    } else {
+      clickedItem.style.backgroundColor = selectedColor; // Set selected color
+    }
+  });
+
+  // Clears the grid and puts eraserState to inactive
+  clearButton.addEventListener("click", function () {
+    item.style.backgroundColor = "whitesmoke";
+    eraserState = false;
+  });
+
+  // When clicking colorPicker, eraserState is inactive
+  colorPicker.addEventListener("click", function () {
+    eraserState = false;
+  });
+});
