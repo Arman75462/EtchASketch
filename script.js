21d4651a552d0e95f673e72204ff70e8ed7b5a86
Arman <armangulistani@gmail.com>
2023-09-10 09:44:17 -0400

    Grid-size works, performance optimizaton and hovering/clicking works for coloring/erasing

--- script.js
+++ script.js
@@ -1,55 +1,184 @@
 "use strict";
 
-/* Selectors */
-const container = document.querySelector(".main__grid");
+// Selectors
+const grid = document.querySelector(".main__grid");
 const colorPicker = document.getElementById("color-picker");
 const eraser = document.getElementById("eraser");
 const clearButton = document.getElementById("clear-button");
+const gridSizePicker = document.getElementById("grid-size-picker");
+const gridSizeText = document.querySelector(".main__show-grid-size");
 
-// Based on chosen color, change the circle's color
+// Debounce function to delay the execution of a function
+function debounce(func, delay) {
+  let timerId;
+  return function () {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, arguments);
+    }, delay);
+  };
+}
+
+// When the user picks a color, update the color picker's display
 colorPicker.addEventListener("input", () => {
   const selectedColor = colorPicker.value;
   colorPicker.style.backgroundColor = selectedColor;
 });
 
-// Create 256 div elements and add them to the container
-for (let i = 1; i <= 4096; i++) {
-  const div = document.createElement("div");
-  div.className = `main__grid-items grid-item-${i}`; // Add both classes
-  container.appendChild(div);
+// Function to set up the drawing grid based on the selected grid size
+function makeEtchWork() {
+  const desiredWidth = 640; // Desired width for the grid in pixels
+  const gridSize = gridSizePicker.value;
+
+  // Calculate the size of each column and row to fit the desired width
+  const cellSize = desiredWidth / gridSize;
+
+  // Adjust the grid's columns and rows based on the selected size
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
+
+  // Create grid cells (divs) based on the selected size
+  for (let i = 1; i <= gridSize ** 2; i++) {
+    const div = document.createElement("div");
+    div.className = `main__grid-items grid-item-${i}`;
+    grid.appendChild(div);
+  }
+
+  // Add interactivity to the grid cells
+  const gridItems = document.querySelectorAll(".main__grid-items");
+
+  let isDrawing = false;
+
+  gridItems.forEach((item) => {
+    let eraserState = false;
+
+    // Toggle the eraser on/off when the eraser button is clicked
+    eraser.addEventListener("click", function () {
+      eraserState = !eraserState;
+    });
+
+    item.addEventListener("mousedown", function () {
+      isDrawing = true;
+    });
+
+    item.addEventListener("mouseup", function () {
+      isDrawing = false;
+    });
+
+    item.addEventListener("mouseenter", function (event) {
+      if (isDrawing) {
+        const selectedColor = colorPicker.value;
+        const clickedItem = event.target;
+
+        if (eraserState) {
+          clickedItem.style.backgroundColor = "whitesmoke";
+        } else {
+          clickedItem.style.backgroundColor = selectedColor;
+        }
+      }
+    });
+
+    clearButton.addEventListener("click", function () {
+      item.style.backgroundColor = "whitesmoke";
+      eraserState = false;
+    });
+
+    colorPicker.addEventListener("click", function () {
+      eraserState = false;
+    });
+  });
 }
 
-const gridItems = document.querySelectorAll(".main__grid-items");
+// Create a debounced version of makeEtchWork with a 300ms delay
+const debouncedMakeEtchWork = debounce(makeEtchWork, 300);
 
-// Depending on clicked button, executes order
-gridItems.forEach((item) => {
-  let eraserState = false;
+// When the user selects a new grid size, update the display and redraw the grid
+gridSizePicker.addEventListener("input", function () {
+  const sliderValue = gridSizePicker.value; // Get the current slider value
 
-  eraser.addEventListener("click", function () {
-    eraserState = !eraserState; // Toggle eraser state
-  });
+  // Update the display element to show the selected grid size
+  gridSizeText.textContent = sliderValue;
 
-  item.addEventListener("mousemove", function (event) {
-    const selectedColor = colorPicker.value;
-    item.style.backgroundColor = selectedColor;
+  // Clear the existing grid cells
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
 
-    const clickedItem = event.target;
+  // An array of grid sizes to execute makeEtchWork for
+  const gridSizesToExecuteMakeGridWork = [
+    "64",
+    "63",
+    "62",
+    "61",
+    "60",
+    "59",
+    "58",
+    "57",
+    "56",
+    "55",
+    "54",
+    "53",
+    "52",
+    "51",
+    "50",
+    "49",
+    "48",
+    "47",
+    "46",
+    "45",
+    "44",
+    "43",
+    "42",
+    "41",
+    "40",
+    "39",
+    "38",
+    "37",
+    "36",
+    "35",
+    "34",
+    "33",
+    "32",
+    "31",
+    "30",
+    "29",
+    "28",
+    "27",
+    "26",
+    "25",
+    "24",
+    "23",
+    "22",
+    "21",
+    "20",
+    "19",
+    "18",
+    "17",
+    "16",
+    "15",
+    "14",
+    "13",
+    "12",
+    "11",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+  ];
 
-    if (eraserState) {
-      clickedItem.style.backgroundColor = "whitesmoke"; // Erase if eraser is active
-    } else {
-      clickedItem.style.backgroundColor = selectedColor; // Set selected color
+  // Execute makeEtchWork when the selected size matches a value in the array
+  gridSizesToExecuteMakeGridWork.forEach((value) => {
+    if (sliderValue === value) {
+      debouncedMakeEtchWork(); // Use the debounced function to delay execution
     }
   });
-
-  // Clears the grid and puts eraserState to inactive
-  clearButton.addEventListener("click", function () {
-    item.style.backgroundColor = "whitesmoke";
-    eraserState = false;
-  });
-
-  // When clicking colorPicker, eraserState is inactive
-  colorPicker.addEventListener("click", function () {
-    eraserState = false;
-  });
 });
+
+// Call makeEtchWork at the end of your code to ensure it runs after everything else.
+makeEtchWork();
